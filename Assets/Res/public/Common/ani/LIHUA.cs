6e80c669a3071b5b4119cb11c9ead6fc931e9fd0
zhangyisong <zhangyisong@thvr.com>
2024-10-16 01:27:46 +0800

    initial

---
+++ Assets/Res/public/Common/ani/LIHUA.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LIHUA : MonoBehaviour
+{
+    public GameObject Panel;
+    public void OnWeiStart() 
+    {
+        Panel.SetActive(true);
+    }
+}
